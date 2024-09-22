a23b074b1fba1b6f7f9b7e2ce71af4c8ba8316b9
Yael <yael.rodriguez@iteso.mx>
2024-09-21 18:12:53 -0600

    Solio copie la tarea, jaja

---
+++ README.md
@@ -0,0 +1,5 @@
+# Proyecto de back, ja jas
+
+`npm run scripts` genera la carpeta dist
+`npm run dev` permite ver cambios en tiempo real
+`npm start` corre todo con normalidad
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import routes from './routes';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get('', (req, res) => {
+    res.send('AAA!')
+})
+
+app.use(routes);
+
+app.listen(port, () => {
+    console.log(`Puerto ${port}`)
+})
