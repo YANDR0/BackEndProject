0d273f1390b885a297bcac03c886d5455250ab85
Maigol <migueltorrespvr2@gmail.com>
2024-10-23 17:05:47 -0600

    CORS xd

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import { connect } from 'mongoose';
 import { config } from 'dotenv';
 config();
 import routes from './routes';
-import { serve, setup} from 'swagger-ui-express'
+import { serve, setup } from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
-import swaggerConfig from "./../swagger.config.json"
-
+import swaggerConfig from "./../swagger.config.json";
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,14 +14,14 @@ const port = process.env.PORT || 3000;
 const dbUrl = process.env.DB_URL;
 console.log('Mongo URL: ', dbUrl);
 
+app.use(cors());
+
 // Middleware para aceptar JSON
 app.use(express.json());
 app.use(routes);
 
 const swaggerDocs = swaggerJSDoc(swaggerConfig);
 app.use('/swagger', serve, setup(swaggerDocs))
-
-
 
 connect(dbUrl as string).then(res => {
     console.log('Ya se conectó la base');
